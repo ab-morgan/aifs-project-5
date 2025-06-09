052541fb47392a8079670dfa88ff427c456f081a
Katherine Koopmans <katherineemuir@gmail.com>
2025-06-09 15:12:37 -0400

    job listings extracted

--- data/USAJobsData/api_requests.py
+++ data/USAJobsData/api_requests.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 
 # API configuration
 api_key = "M4j0Kq+yHXJQ+DC79jIODphkl0efmEOLFMZty6MeFFA="
@@ -7,9 +8,13 @@ api_url = "https://data.usajobs.gov/api/Search"
 head = {'Authorization-Key': api_key} 
      
 
-# Replace this with full list of job codes
-# extract from usajobs-code-list.json
-code_list = ['0804']
+# list of job codes extracted from file
+with open('data/USAJobsData/usajobs-code-list.json') as f:
+    job_code_data = json.load(f)
+
+codelist = []
+for code in job_code_data['CodeList'][0]['ValidValue']:
+    codelist.append(code["Code"])
 
 # function to call API
 def call_api(job_code):
@@ -24,10 +29,11 @@ def call_api(job_code):
 
 # loop over list to call api
 results = {}
-for value in code_list:
+for value in codelist:
     data = call_api(value)
     if data is not None:
         results[value] = data
+    time.sleep(1)
 
-with open("test.json", "w") as f:
+with open("data/USAJobsData/all-listings.json", "w") as f:
     json.dump(results, f)
